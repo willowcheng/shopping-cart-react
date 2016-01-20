612eec5cfb434518308d668b9e3d260a076085a7
willowcheng <e.wingus@gmail.com>
2016-01-20 10:14:12 -0500

    Add dispatcher and fetching data process in app.js

--- js/app.js
+++ js/app.js
@@ -1,3 +1,20 @@
 /**
  * Created by liuc on 1/20/2016.
  */
+
+window.React = require('react');
+var ProductData = require('./ProductData');
+var CartAPI = require('./utils/CartAPI');
+var FluxCartApp = require('./components/FluxCartApp.react');
+
+// Load Mock Product Data into localStorage
+ProductData.init();
+
+// Load Mock API Call
+CartAPI.getProductData();
+
+// Render FluxCartApp Controller View
+React.render(
+    <FluxCartApp />,
+    document.getElementById('flux-cart')
+);
--- js/dispatcher/AppDispatcher.js
+++ js/dispatcher/AppDispatcher.js
@@ -1,3 +1,18 @@
 /**
  * Created by liuc on 1/20/2016.
  */
+
+var Dispatcher = require('flux').Dispatcher;
+
+// Create dispatcher instance
+var AppDispatcher = new Dispatcher();
+
+// Convenience method to handle dispatch requests
+AppDispatcher.handleAction = function(action) {
+    this.dispatch({
+        source: 'VIEW_ACTION',
+        action: action
+    });
+};
+
+module.exports = AppDispatcher;
